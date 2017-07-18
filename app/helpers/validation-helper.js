b4e98b43e79bd8db5eed7d48700d97f9a89391ef
dnigam <devvrat.nigam@sony.com>
2017-07-18 15:22:58 -0700

    Editor config and sigup form error messages added

--- app/helpers/validation-helper.js
+++ app/helpers/validation-helper.js
@@ -1,20 +1,22 @@
 import Ember from 'ember';
 
 const {
-  isPresent,
-  typeOf,
-  isEmpty,
-  isNone
+    isPresent,
+    typeOf,
+    isEmpty,
+    isNone
 } = Ember;
 
 export function isInputValid(inputText) {
-  return isPresent(inputText) && typeOf(inputText) === 'string' && !isEmpty(inputText) && !isNone(inputText);
+    return isPresent(inputText) && typeOf(inputText) === 'string' &&
+        !isEmpty(inputText) && !isNone(inputText) && /^[a-z\d\-_\s]+$/i.test(inputText);
 }
 
 export function isEmailDomainValid(inputEmail) {
-  return /.+@csu.fullerton\.edu$/.test(inputEmail);
+    //return /.+@csu.fullerton\.edu$/.test(inputEmail);
+    return /.+.\edu$/.test(inputEmail);
 }
 
 export function isInputOfCorrectLength(input, minLength, maxLength) {
-  return input.length >= minLength && input.length <= maxLength;
+    return input.length >= minLength && input.length <= maxLength;
 }
