05a979dbd2f658cc232f9fa744ff29f45eea117e
dnigam <devvrat.nigam@sony.com>
2017-09-18 03:23:40 -0700

    Attemp quiz animation

--- app/components/all-quizzes-info/component.js
+++ app/components/all-quizzes-info/component.js
@@ -2,10 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     isStudent: true,
-    classNames: ['col s12 m5 info-questions-card'],
+    classNames: ['col s12 m5'],
     actions: {
         attemptQuiz(quiz) {
             console.log(quiz);
+            Ember.$('.info-questions-card').each(function(o) {
+                $(this).fadeOut(1000);
+            });
+            this.set('dynamic-component', 'quiz-dailog');
+        },
+
+        hideQuiz() {
+            this.$().fadeOut(1000);
         }
     }
 });
