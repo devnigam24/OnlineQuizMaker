98d37f6fcc90349226f9d003551f75dacb150818
dnigam <devvrat.nigam@sony.com>
2017-11-01 17:39:45 -0700

    Removing db from project folder

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.JSONAPIAdapter.extend({
     namespace: '',
     host: 'http://localhost:3000',
-    headers: function(data) {
+    headers: function() {
         var headers = {};
         headers['Content-Type'] = 'application/vnd.api+json';
         return headers;
