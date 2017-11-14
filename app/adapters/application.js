2373e4aa25a68b0f037dad1e628fe1b3319bc634
devnigam24 <devnigam24@gmail.com>
2017-11-14 15:47:38 -0800

    fix for ember data save

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,9 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
     namespace: '',
-    host: 'http://localhost:3000'
+    host: 'http://localhost:3000',
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    }
 });
