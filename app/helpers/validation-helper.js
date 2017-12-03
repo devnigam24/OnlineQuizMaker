08e2de6fd8bab43f0c9fb48830fc3c0a56429aff
devnigam24 <devnigam24@gmail.com>
2017-12-02 19:25:28 -0800

    deleting commented lines from validation

--- app/helpers/validation-helper.js
+++ app/helpers/validation-helper.js
@@ -10,31 +10,25 @@ const {
 export function isInputValidText(inputText) {
     return isPresent(inputText) && typeOf(inputText) === 'string' &&
         !isEmpty(inputText) && !isNone(inputText);
-    // return true;
 }
 
 export function isInputValidNumber(inputText) {
     return isPresent(inputText) && !(Number.parseInt(inputText).toString() === "NaN") &&
         !isEmpty(inputText) && !isNone(inputText);
-    // return true;
 }
 
 export function isPhoneNumberFormatValid(inputText) {
     return /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(inputText);
-    // return true;
 }
 
-export function isEmailDomainValid(/*inputEmail*/) {
-    // return /.+@csu.fullerton\.edu$/.test(inputEmail);
-    return true;
+export function isEmailDomainValid(inputEmail) {
+    return /.+@csu.fullerton\.edu$/.test(inputEmail);
 }
 
 export function isInputOfCorrectLength(input, minLength, maxLength) {
     return input.length >= minLength && input.length <= maxLength;
-    // return true;
 }
 
 export function isInputDoesNotHasSpecialChars(input) {
     return /^[a-z\d\-_\s]+$/i.test(input);
-    // return true;
 }
