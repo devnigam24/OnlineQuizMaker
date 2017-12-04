ed7e47dfd2bf4ffb8b43360772940e07e5569a52
devnigam24 <devnigam24@gmail.com>
2017-12-04 15:07:33 -0800

    File added for mock user data

---
+++ app/helpers/internationalization.js
@@ -0,0 +1,23 @@
+const Internationalization = {
+  description: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet ex mattis, porttitor nunc eget, ornare lacus. Cras tincidunt nisi est, sit amet pharetra quam tempor vitae. Nullam lacus sem, lacinia in ipsum sed, commodo sagittis neque. Aliquam viverra cursus nibh, at rhoncus lectus varius id. Nam vitae felis varius, varius nunc in, elementum velit. Pellentesque quis lobortis dolor. Suspendisse potenti. Donec egestas ullamcorper consequat. Suspendisse tincidunt lacus dolor, nec vehicula nisi posuere id. Proin gravida at turpis sed interdum. Mauris lacinia justo quis orci dignissim, quis iaculis libero eleifend. Duis tortor turpis, fermentum id accumsan egestas, posuere in lectus. Mauris ac sapien quis dui congue tincidunt. Nunc at urna vulputate, bibendum enim et, scelerisque ante. Nam eu commodo nisl.',
+      'Ut pharetra laoreet ipsum, sed maximus tortor interdum nec. Nulla congue imperdiet lorem, nec dignissim nulla cursus at. Quisque turpis metus, egestas id convallis malesuada, tristique quis tortor. In hendrerit condimentum nisi in convallis. Sed massa quam, porta quis volutpat non, tincidunt sed erat. Mauris pulvinar, ipsum eget accumsan luctus, ante enim lacinia diam, sed elementum elit ante sodales mauris. Aenean fringilla, augue vehicula ullamcorper porttitor, purus orci suscipit lorem, porta luctus velit nulla et metus. Ut lorem neque, hendrerit sed tincidunt id, venenatis imperdiet ligula.',
+      'Morbi suscipit placerat sem, et semper ante. Sed a lorem feugiat libero luctus elementum a a diam. Pellentesque vitae ipsum in magna lacinia fringilla eu vel arcu. Nulla consequat dignissim neque sed semper. Sed augue metus, tempor sit amet ligula nec, molestie sollicitudin leo. Phasellus ligula velit, vestibulum eget orci vel, hendrerit iaculis urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam ac magna elementum, scelerisque dui sit amet, laoreet est. Cras volutpat fringilla dolor sed eleifend. Maecenas felis massa, laoreet vel dolor auctor, ullamcorper facilisis massa. Sed varius ex non finibus eleifend. Nullam nec elit tellus. Nunc pulvinar lacinia felis, vel lobortis nulla egestas quis. Praesent bibendum justo viverra tellus porta, sed sagittis quam convallis. Phasellus volutpat sed lorem vitae molestie.',
+      'Donec viverra purus quis diam pharetra, a pellentesque erat maximus. Aenean convallis eget ipsum sit amet dapibus. Vivamus ipsum nunc, consequat vitae vestibulum sed, bibendum a metus. Nullam vitae leo et metus ornare sodales et at leo. Nullam sagittis justo ut suscipit iaculis. Pellentesque sodales at risus sed rhoncus. Donec efficitur quis elit quis dapibus. Aliquam erat volutpat. Suspendisse pretium orci leo, id vulputate justo venenatis eget. Vivamus non hendrerit est. Mauris lectus justo, congue a velit quis, maximus iaculis nunc. Aenean diam est, faucibus et maximus in, sollicitudin eu nunc. Integer aliquam rhoncus dolor, sed malesuada mauris pellentesque ac. Praesent id nibh consectetur, fringilla ipsum quis, auctor enim. Mauris consectetur fermentum ligula, sed cursus elit pharetra id.',
+      'Vestibulum in consequat neque, vel venenatis erat. Sed mauris purus, rutrum ut elementum ac, gravida at nunc. Suspendisse quis purus sit amet purus mollis vestibulum vitae vitae sem. Etiam finibus eu turpis sed feugiat. Phasellus luctus in quam id molestie. In gravida mauris quis neque mattis, eu lacinia orci vehicula. Nullam ipsum diam, porttitor vel augue ac, fringilla tristique tortor. Phasellus vel est metus. Aliquam tristique facilisis ante, nec egestas felis tempor in. Donec sit amet facilisis ante. Duis feugiat, libero eu sagittis consequat, diam enim ultricies lectus, eget efficitur est felis ac libero. Donec posuere finibus tempor. Integer commodo leo diam, eu lobortis neque molestie et.'
+  ],
+
+  streetAddress: [
+    'P.O. Box 279, 7334 Feugiat St',
+    'P.O. Box 280, 7335 Feugiat St',
+    'P.O. Box 278, 7336 Feugiat St',
+    'P.O. Box 276, 7337 Feugiat St',
+    'P.O. Box 277, 7338 Feugiat St'
+  ],
+
+  pincodes: ['92831','92832', '99875', '93267', '94567'],
+
+  cities: ['Fullerton','Santa Ana', 'Orange', 'LA', 'Anahiem']
+};
+
+export default Internationalization;
